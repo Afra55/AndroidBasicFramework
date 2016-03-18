b7b41eb5ed4b265aa50fae565238d9623c05071f
杨帅 <yangshuai@funguide.com.cn>
2016-03-18 16:49:58 +0800

    版本迭代temp1.0

--- app/src/main/java/com/afra55/baseclient/base/CusstomApplication.java
+++ app/src/main/java/com/afra55/baseclient/base/CusstomApplication.java
@@ -2,6 +2,14 @@ package com.afra55.baseclient.base;
 
 import android.app.Application;
 
+import com.facebook.common.internal.Supplier;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.backends.okhttp.OkHttpImagePipelineConfigFactory;
+import com.facebook.imagepipeline.cache.MemoryCacheParams;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.squareup.okhttp.OkHttpClient;
+
+
 /**
  * Created by yangshuai in the 10:37 of 2016.01.05 .
  */
@@ -10,7 +18,23 @@ public class CusstomApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//
+//        Supplier<MemoryCacheParams> bitmapCacheParamsSupplier = new Supplier<MemoryCacheParams>() {
+//            @Override
+//            public MemoryCacheParams get() {
+//                return new MemoryCacheParams();
+//            }
+//        };
+//
+//        ImagePipelineConfig config = OkHttpImagePipelineConfigFactory
+//                .newBuilder(getApplicationContext(), okHttpClient)
+//                .setBitmapMemoryCacheParamsSupplier(bitmapCacheParamsSupplier)
+//                .build();
+//        Fresco.initialize(this, config);
+
         /* 异常捕获(debug 时不捕获异常) */
         Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler(this));
+
     }
 }
---
+++ app/src/main/java/com/afra55/baseclient/util/FrescoHelper.java
@@ -0,0 +1,20 @@
+package com.afra55.baseclient.util;
+
+/**
+ * Created by yangshuai in the 14:33 of 2016.03.18 .
+ */
+public class FrescoHelper {
+
+    private static final String TAG = "FrescoHelper";
+
+    private FrescoHelper() {
+
+    }
+
+    public static FrescoHelper getInstance() {
+        return new FrescoHelper();
+    }
+
+
+
+}
