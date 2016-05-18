ae14a776f172a1e1459b4199d12230f3791b771f
Afra55 <1937525930@qq.com>
2016-05-17 18:20:29 -0700

    ui file util.

---
+++ CommontUtils/src/main/java/com/afra55/commontutils/file/FileUtil.java
@@ -0,0 +1,242 @@
+package com.afra55.commontutils.file;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
+
+import com.afra55.commontutils.string.StringUtil;
+
+import java.io.File;
+import java.util.Locale;
+
+public class FileUtil {
+    private static final String TAG = "FileUtil";
+
+    public static boolean hasExtentsion(String filename) {
+        int dot = filename.lastIndexOf('.');
+        if ((dot > -1) && (dot < (filename.length() - 1))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // 获取文件扩展名
+    public static String getExtensionName(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot > -1) && (dot < (filename.length() - 1))) {
+                return filename.substring(dot + 1);
+            }
+        }
+        return "";
+    }
+
+    // 获取文件名
+    public static String getFileNameFromPath(String filepath) {
+        if ((filepath != null) && (filepath.length() > 0)) {
+            int sep = filepath.lastIndexOf('/');
+            if ((sep > -1) && (sep < filepath.length() - 1)) {
+                return filepath.substring(sep + 1);
+            }
+        }
+        return filepath;
+    }
+
+    // 获取不带扩展名的文件名
+    public static String getFileNameNoEx(String filename) {
+        if ((filename != null) && (filename.length() > 0)) {
+            int dot = filename.lastIndexOf('.');
+            if ((dot > -1) && (dot < (filename.length()))) {
+                return filename.substring(0, dot);
+            }
+        }
+        return filename;
+    }
+
+    public static String getMimeType(String filePath) {
+        if (TextUtils.isEmpty(filePath)) {
+            return "";
+        }
+        String type = null;
+        String extension = getExtensionName(filePath.toLowerCase());
+        if (!TextUtils.isEmpty(extension)) {
+            MimeTypeMap mime = MimeTypeMap.getSingleton();
+            type = mime.getMimeTypeFromExtension(extension);
+        }
+        Log.i(TAG, "url:" + filePath + " " + "type:" + type);
+
+        // FIXME
+        if (StringUtil.isEmpty(type) && filePath.endsWith("aac")) {
+            type = "audio/aac";
+        }
+
+        return type;
+    }
+
+    public enum SizeUnit {
+        Byte,
+        KB,
+        MB,
+        GB,
+        TB,
+        Auto,
+    }
+
+    public static String formatFileSize(long size) {
+        return formatFileSize(size, SizeUnit.Auto);
+    }
+
+    public static String formatFileSize(long size, SizeUnit unit) {
+        if (size < 0) {
+            return "0B";
+        }
+
+        final double KB = 1024;
+        final double MB = KB * 1024;
+        final double GB = MB * 1024;
+        final double TB = GB * 1024;
+        if (unit == SizeUnit.Auto) {
+            if (size < KB) {
+                unit = SizeUnit.Byte;
+            } else if (size < MB) {
+                unit = SizeUnit.KB;
+            } else if (size < GB) {
+                unit = SizeUnit.MB;
+            } else if (size < TB) {
+                unit = SizeUnit.GB;
+            } else {
+                unit = SizeUnit.TB;
+            }
+        }
+
+        switch (unit) {
+            case Byte:
+                return size + "B";
+            case KB:
+                return String.format(Locale.US, "%.2fKB", size / KB);
+            case MB:
+                return String.format(Locale.US, "%.2fMB", size / MB);
+            case GB:
+                return String.format(Locale.US, "%.2fGB", size / GB);
+            case TB:
+                return String.format(Locale.US, "%.2fPB", size / TB);
+            default:
+                return size + "B";
+        }
+    }
+
+    /**
+     * 根据文件后缀名获得对应的MIME类型。
+     *
+     * @param file
+     */
+    public static String getMimeType(File file) {
+
+        String type = "*/*";
+        String fName = file.getName();
+        //获取后缀名前的分隔符"."在fName中的位置。
+        int dotIndex = fName.lastIndexOf(".");
+        if (dotIndex < 0) {
+            return type;
+        }
+        /* 获取文件的后缀名*/
+        String end = fName.substring(dotIndex, fName.length()).toLowerCase();
+        if (end == "") return type;
+        //在MIME和文件类型的匹配表中找到对应的MIME类型。
+        for (int i = 0; i < MIME_MapTable.length; i++) { //MIME_MapTable??在这里你一定有疑问，这个MIME_MapTable是什么？
+            if (end.equals(MIME_MapTable[i][0]))
+                type = MIME_MapTable[i][1];
+        }
+        return type;
+    }
+
+    public static String getMimeTypeByName(String fName) {
+        String type = "*/*";
+        //获取后缀名前的分隔符"."在fName中的位置。
+        int dotIndex = fName.lastIndexOf(".");
+        if (dotIndex < 0) {
+            return type;
+        }
+        /* 获取文件的后缀名*/
+        String end = fName.substring(dotIndex, fName.length()).toLowerCase();
+        if (TextUtils.isEmpty(end)) return type;
+        //在MIME和文件类型的匹配表中找到对应的MIME类型。
+        for (int i = 0; i < MIME_MapTable.length; i++) { //MIME_MapTable??在这里你一定有疑问，这个MIME_MapTable是什么？
+            if (end.equals(MIME_MapTable[i][0]))
+                type = MIME_MapTable[i][1];
+        }
+        return type;
+    }
+
+    private static final String[][] MIME_MapTable = {
+            //{后缀名，MIME类型}
+            {".3gp", "video/3gpp"},
+            {".apk", "application/vnd.android.package-archive"},
+            {".asf", "video/x-ms-asf"},
+            {".avi", "video/x-msvideo"},
+            {".bin", "application/octet-stream"},
+            {".bmp", "image/bmp"},
+            {".c", "text/plain"},
+            {".class", "application/octet-stream"},
+            {".conf", "text/plain"},
+            {".cpp", "text/plain"},
+            {".doc", "application/msword"},
+            {".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+            {".xls", "application/vnd.ms-excel"},
+            {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+            {".exe", "application/octet-stream"},
+            {".gif", "image/gif"},
+            {".gtar", "application/x-gtar"},
+            {".gz", "application/x-gzip"},
+            {".h", "text/plain"},
+            {".htm", "text/html"},
+            {".html", "text/html"},
+            {".jar", "application/java-archive"},
+            {".java", "text/plain"},
+            {".jpeg", "image/jpeg"},
+            {".jpg", "image/jpeg"},
+            {".js", "application/x-JavaScript"},
+            {".log", "text/plain"},
+            {".m3u", "audio/x-mpegurl"},
+            {".m4a", "audio/mp4a-latm"},
+            {".m4b", "audio/mp4a-latm"},
+            {".m4p", "audio/mp4a-latm"},
+            {".m4u", "video/vnd.mpegurl"},
+            {".m4v", "video/x-m4v"},
+            {".mov", "video/quicktime"},
+            {".mp2", "audio/x-mpeg"},
+            {".mp3", "audio/x-mpeg"},
+            {".mp4", "video/mp4"},
+            {".mpc", "application/vnd.mpohun.certificate"},
+            {".mpe", "video/mpeg"},
+            {".mpeg", "video/mpeg"},
+            {".mpg", "video/mpeg"},
+            {".mpg4", "video/mp4"},
+            {".mpga", "audio/mpeg"},
+            {".msg", "application/vnd.ms-outlook"},
+            {".ogg", "audio/ogg"},
+            {".pdf", "application/pdf"},
+            {".png", "image/png"},
+            {".pps", "application/vnd.ms-powerpoint"},
+            {".ppt", "application/vnd.ms-powerpoint"},
+            {".pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+            {".prop", "text/plain"},
+            {".rc", "text/plain"},
+            {".rmvb", "audio/x-pn-realaudio"},
+            {".rtf", "application/rtf"},
+            {".sh", "text/plain"},
+            {".tar", "application/x-tar"},
+            {".tgz", "application/x-compressed"},
+            {".txt", "text/plain"},
+            {".wav", "audio/x-wav"},
+            {".wma", "audio/x-ms-wma"},
+            {".wmv", "audio/x-ms-wmv"},
+            {".wps", "application/vnd.ms-works"},
+            {".xml", "text/plain"},
+            {".z", "application/x-compress"},
+            {".zip", "application/x-zip-compressed"},
+            {"", "*/*"}
+    };
+
+}
