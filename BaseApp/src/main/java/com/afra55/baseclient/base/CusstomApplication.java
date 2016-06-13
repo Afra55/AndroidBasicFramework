538fa7ac22bc4375ee96a1781b92729c686e26d8
Afra55 <1937525930@qq.com>
2016-06-13 17:55:27 +0800

    fix bug.

--- BaseApp/src/main/java/com/afra55/baseclient/base/CusstomApplication.java
+++ BaseApp/src/main/java/com/afra55/baseclient/base/CusstomApplication.java
@@ -1,7 +1,6 @@
 package com.afra55.baseclient.base;
 
 import android.app.Application;
-import android.app.LauncherActivity;
 import android.util.Log;
 
 import com.afra55.baseclient.BuildConfig;
@@ -11,12 +10,6 @@ import com.afra55.commontutils.log.LogUtil;
 import com.afra55.commontutils.storage.StorageType;
 import com.afra55.commontutils.storage.StorageUtil;
 import com.afra55.commontutils.sys.ScreenUtil;
-import com.facebook.common.internal.Supplier;
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.imagepipeline.backends.okhttp.OkHttpImagePipelineConfigFactory;
-import com.facebook.imagepipeline.cache.MemoryCacheParams;
-import com.facebook.imagepipeline.core.ImagePipelineConfig;
-import com.squareup.okhttp.OkHttpClient;
 
 
 /**
@@ -36,21 +29,6 @@ public class CusstomApplication extends Application {
         // init log
         String path = StorageUtil.getDirectoryByDirType(StorageType.TYPE_LOG);
         LogUtil.init(path, Log.DEBUG);
-
-//        OkHttpClient okHttpClient = new OkHttpClient();
-//
-//        Supplier<MemoryCacheParams> bitmapCacheParamsSupplier = new Supplier<MemoryCacheParams>() {
-//            @Override
-//            public MemoryCacheParams get() {
-//                return new MemoryCacheParams();
-//            }
-//        };
-//
-//        ImagePipelineConfig config = OkHttpImagePipelineConfigFactory
-//                .newBuilder(getApplicationContext(), okHttpClient)
-//                .setBitmapMemoryCacheParamsSupplier(bitmapCacheParamsSupplier)
-//                .build();
-//        Fresco.initialize(this, config);
 
         /* 异常捕获(debug 时不捕获异常) */
         if (!BuildConfig.DEBUG) {
