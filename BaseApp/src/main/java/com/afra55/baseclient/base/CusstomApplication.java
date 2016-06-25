b8d0d3cf5e50d9276551816c4eb88ef5ce0eb3bf
Afra55 <1937525930@qq.com>
2016-06-25 16:33:15 +0800

    Open StrictMode mode.

--- BaseApp/src/main/java/com/afra55/baseclient/base/CusstomApplication.java
+++ BaseApp/src/main/java/com/afra55/baseclient/base/CusstomApplication.java
@@ -1,6 +1,7 @@
 package com.afra55.baseclient.base;
 
 import android.app.Application;
+import android.os.StrictMode;
 import android.util.Log;
 
 import com.afra55.baseclient.BuildConfig;
@@ -33,6 +34,20 @@ public class CusstomApplication extends Application {
         /* 异常捕获(debug 时不捕获异常) */
         if (!BuildConfig.DEBUG) {
             AppCrashHandler.getInstance(this, MainActivity.class);
+        } else {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectAll()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .penaltyDialog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
         }
 
     }
