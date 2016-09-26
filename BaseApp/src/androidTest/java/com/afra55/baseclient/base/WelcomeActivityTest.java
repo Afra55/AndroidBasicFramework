8156c6afdd918d3fdded4e905ac67038e1f1bf5a
Afra55 <1937525930@qq.com>
2016-09-26 22:50:17 +0800

    Android  使用 Espresso Test Recorder 自动生成 UI 测试代码;

---
+++ BaseApp/src/androidTest/java/com/afra55/baseclient/base/WelcomeActivityTest.java
@@ -0,0 +1,124 @@
+package com.afra55.baseclient.base;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.afra55.baseclient.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class WelcomeActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void welcomeActivityTest() {
+        ViewInteraction linearLayout = onView(
+                allOf(withId(R.id.main_bottom_menu_community),
+                        withParent(withId(R.id.main_bottom_menu_content)),
+                        isDisplayed()));
+        linearLayout.perform(click());
+
+        ViewInteraction textInputEditText = onView(
+                allOf(withId(R.id.commnunity_translate_et),
+                        withParent(withId(R.id.commnunity_translate_layout)),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText("xxxx"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.commnunity_translate_btn), withText("To Translate"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.commnunity_translate_result), withText("XXXX "),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                2),
+                        isDisplayed()));
+        textView.check(matches(withText("XXXX ")));
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.commnunity_translate_btn),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(withId(R.id.commnunity_translate_et), withText("xxxx"),
+                        withParent(withId(R.id.commnunity_translate_layout)),
+                        isDisplayed()));
+        textInputEditText2.perform(click());
+
+        ViewInteraction textInputEditText3 = onView(
+                allOf(withId(R.id.commnunity_translate_et),  //withText("xxxx"),
+                        withParent(withId(R.id.commnunity_translate_layout)),
+                        isDisplayed()));
+        textInputEditText3.perform(replaceText("Victor"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.commnunity_translate_btn), withText("To Translate"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.commnunity_translate_result), withText("胜利者 "),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                2),
+                        isDisplayed()));
+        textView2.check(matches(withText("胜利者 ")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
