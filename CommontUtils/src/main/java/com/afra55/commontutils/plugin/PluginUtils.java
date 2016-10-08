80f0d27ca68558fb73d7bf17243ea158b06af76e
Afra55 <1937525930@qq.com>
2016-10-08 11:26:15 +0800

    Plugin init 2.

--- CommontUtils/src/main/java/com/afra55/commontutils/plugin/PluginUtils.java
+++ CommontUtils/src/main/java/com/afra55/commontutils/plugin/PluginUtils.java
@@ -7,10 +7,15 @@ import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
 
+import com.afra55.commontutils.R;
 import com.afra55.commontutils.bean.PluginInfoBean;
 import com.afra55.commontutils.storage.StorageUtil;
 
+import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import dalvik.system.DexClassLoader;
 
 /**
  * Created by Victor Yang on 2016/10/8.
@@ -61,5 +66,32 @@ public class PluginUtils {
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * 加载apk获得内部资源 ID, 然后使用 插件的 Resources 获取资源
+     *
+     * @param context context
+     * @param pluginInfoBean  PluginInfoBean
+     * @throws Exception
+     */
+    private int dynamicLoadApk(Context context, PluginInfoBean pluginInfoBean, String resourceName) throws Exception {
+        File dexOutputDir = context.getDir("dex", 0);
+        if (dexOutputDir == null && !dexOutputDir.mkdir()) {
+            throw new Resources.NotFoundException("dex dir not found");
+        }
+        //参数：1、包含dex的apk文件或jar文件的路径，2、apk、jar解压缩生成dex存储的目录，3、本地library库目录，一般为null，4、父ClassLoader
+        DexClassLoader dexClassLoader = new DexClassLoader(
+                StorageUtil.getPluginDir()
+                , dexOutputDir.getPath()
+                , null
+                , ClassLoader.getSystemClassLoader());
+
+        //通过使用apk自己的类加载器，反射出R类中相应的内部类进而获取我们需要的资源id
+        Class<?> clazz = dexClassLoader.loadClass(pluginInfoBean.getPackageName() + ".R$mipmap");
+
+        //得到名为 resourceName 字段
+        Field field = clazz.getDeclaredField(resourceName);
+        return field.getInt(R.id.class);
     }
 }
