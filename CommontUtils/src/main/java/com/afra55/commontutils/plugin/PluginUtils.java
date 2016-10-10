85400ae0c69d2b77ab6120ffee7c7efe402798fe
Afra55 <1937525930@qq.com>
2016-10-10 16:08:26 +0800

    Plugin init 3.

--- CommontUtils/src/main/java/com/afra55/commontutils/plugin/PluginUtils.java
+++ CommontUtils/src/main/java/com/afra55/commontutils/plugin/PluginUtils.java
@@ -1,17 +1,21 @@
 package com.afra55.commontutils.plugin;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.content.res.Resources;
+import android.os.Bundle;
 
 import com.afra55.commontutils.R;
 import com.afra55.commontutils.bean.PluginInfoBean;
+import com.afra55.commontutils.log.LogUtil;
 import com.afra55.commontutils.storage.StorageUtil;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
@@ -23,6 +27,8 @@ import dalvik.system.DexClassLoader;
  */
 
 public class PluginUtils {
+
+    private static final String TAG = PluginUtils.class.getSimpleName();
 
     /**
      * 获取未安装apk的信息
@@ -41,6 +47,10 @@ public class PluginUtils {
             pluginInfoBean.setIcon(pm.getApplicationIcon(appInfo));//图标
             pluginInfoBean.setPluginName(pm.getApplicationLabel(appInfo).toString());//app名称
             pluginInfoBean.setPluginPackageName(appInfo.packageName);//包名
+            if ((pkgInfo.activities != null)
+                    && (pkgInfo.activities.length > 0)) {
+                pluginInfoBean.setPluginMainClassName(pkgInfo.activities[0].name);
+            }
         }
         return pluginInfoBean;
     }
@@ -71,8 +81,8 @@ public class PluginUtils {
     /**
      * 加载apk获得内部资源 ID, 然后使用 插件的 Resources 获取资源
      *
-     * @param context context
-     * @param pluginInfoBean  PluginInfoBean
+     * @param context        context
+     * @param pluginInfoBean PluginInfoBean
      * @throws Exception
      */
     private int dynamicLoadApk(Context context, PluginInfoBean pluginInfoBean, String resourceName) throws Exception {
@@ -93,5 +103,60 @@ public class PluginUtils {
         //得到名为 resourceName 字段
         Field field = clazz.getDeclaredField(resourceName);
         return field.getInt(R.id.class);
+    }
+
+    /**
+     * 启动 插件的 Activity.
+     * @param context Context
+     * @param archiveFilePath 插件路径
+     * @param className 需要启动的 Activity 名字（例: com.afra55.baseclient.base.MainActivity）
+     * @param bundle Bundle
+     * @param needAgencyTargetActivity 是否代理目标 Activity ，
+     *                                 如果代理则需要实现 setProxy(Activity proxyActivity) 方法，
+     *                                 并代理全部的生命周期及其他。
+     */
+    public static void launchTargetActivity(
+            Context context
+            , String archiveFilePath
+            , final String className
+            , Bundle bundle
+            , boolean needAgencyTargetActivity) {
+        LogUtil.d(TAG, "start launchTargetActivity, className=" + className);
+        File dexOutputDir = context.getDir("dex", 0);
+        final String dexOutputPath = dexOutputDir.getAbsolutePath();
+        ClassLoader localClassLoader = ClassLoader.getSystemClassLoader();
+        DexClassLoader dexClassLoader = new DexClassLoader(
+                archiveFilePath
+                , dexOutputPath
+                , null
+                , localClassLoader);
+        try {
+            Class<?> localClass = dexClassLoader.loadClass(className);
+            Constructor<?> localConstructor = localClass
+                    .getConstructor(new Class[]{});
+            Object instance = localConstructor.newInstance(new Object[]{});
+            LogUtil.d(TAG, "instance = " + instance);
+
+            if (needAgencyTargetActivity) {
+                // 需要实现 setProxy(Activity proxyActivity) 方法
+                // 如果实现这个方法，则由传入的 context 代理打开的 activity。
+                Method setProxy = localClass.getMethod("setProxy",
+                        new Class[]{Activity.class});
+                setProxy.setAccessible(true);
+                setProxy.invoke(instance, new Object[]{context});
+            }
+
+            Method onCreate = localClass.getDeclaredMethod("onCreate",
+                    new Class[]{Bundle.class});
+            onCreate.setAccessible(true);
+
+            // bundle 会由 onCreate(Bundle savedInstanceState)中的 savedInstanceState 接收
+            if (bundle == null) {
+                bundle = new Bundle();
+            }
+            onCreate.invoke(instance, new Object[]{bundle});
+        } catch (Exception e) {
+            LogUtil.e(TAG, "launchTargetActivity", e);
+        }
     }
 }
