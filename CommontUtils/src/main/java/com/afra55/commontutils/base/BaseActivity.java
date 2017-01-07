7060fc170c5155187699d4cbf69eeaaf84a98ea3
Afra55 <1937525930@qq.com>
2017-01-07 20:07:59 +0800

    settings.

--- CommontUtils/src/main/java/com/afra55/commontutils/base/BaseActivity.java
+++ CommontUtils/src/main/java/com/afra55/commontutils/base/BaseActivity.java
@@ -1,11 +1,16 @@
 package com.afra55.commontutils.base;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v4.view.LayoutInflaterFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +18,7 @@ import android.support.v7.app.AppCompatDelegate;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.afra55.commontutils.base.presenter.BaseActivityPresenter;
 import com.afra55.commontutils.base.ui.BaseActivityUI;
@@ -192,4 +198,72 @@ public class BaseActivity extends AppCompatActivity implements BaseActivityUI, O
     public void onFragmentInteraction(String message) {
 
     }
+
+    /**
+     * This utility method handles Up navigation intents by searching for a parent activity and
+     * navigating there if defined. When using this for an activity make sure to define both the
+     * native parentActivity as well as the AppCompat one when supporting API levels less than 16.
+     * when the activity has a single parent activity. If the activity doesn't have a single parent
+     * activity then don't define one and this method will use back button functionality. If "Up"
+     * functionality is still desired for activities without parents then use {@code
+     * syntheticParentActivity} to define one dynamically.
+     * <p/>
+     * Note: Up navigation intents are represented by a back arrow in the top left of the Toolbar in
+     * Material Design guidelines.
+     *
+     * @param currentActivity         Activity in use when navigate Up action occurred.
+     * @param syntheticParentActivity Parent activity to use when one is not already configured.
+     */
+    public static void navigateUpOrBack(Activity currentActivity,
+                                        Class<? extends Activity> syntheticParentActivity) {
+        // Retrieve parent activity from AndroidManifest.
+        Intent intent = NavUtils.getParentActivityIntent(currentActivity);
+
+        // Synthesize the parent activity when a natural one doesn't exist.
+        if (intent == null && syntheticParentActivity != null) {
+            try {
+                intent = NavUtils.getParentActivityIntent(currentActivity, syntheticParentActivity);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (intent == null) {
+            // No parent defined in manifest. This indicates the activity may be used by
+            // in multiple flows throughout the app and doesn't have a strict parent. In
+            // this case the navigation up button should act in the same manner as the
+            // back button. This will result in users being forwarded back to other
+            // applications if currentActivity was invoked from another application.
+            currentActivity.onBackPressed();
+        } else {
+            if (NavUtils.shouldUpRecreateTask(currentActivity, intent)) {
+                // Need to synthesize a backstack since currentActivity was probably invoked by a
+                // different app. The preserves the "Up" functionality within the app according to
+                // the activity hierarchy defined in AndroidManifest.xml via parentActivity
+                // attributes.
+                TaskStackBuilder builder = TaskStackBuilder.create(currentActivity);
+                builder.addNextIntentWithParentStack(intent);
+                builder.startActivities();
+            } else {
+                // Navigate normally to the manifest defined "Up" activity.
+                NavUtils.navigateUpTo(currentActivity, intent);
+            }
+        }
+    }
+
+    /**
+     * Configure this Activity as a floating window, with the given {@code width}, {@code height}
+     * and {@code alpha}, and dimming the background with the given {@code dim} value.
+     */
+    protected void setupFloatingWindow(int width, int height, int alpha, float dim) {
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.width = getResources().getDimensionPixelSize(width);
+        params.height = getResources().getDimensionPixelSize(height);
+        params.alpha = alpha;
+        params.dimAmount = dim;
+        params.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        getWindow().setAttributes(params);
+    }
+
+
 }
