1bcad8405fca093d1418233fd54b17ef56356da1
Afra55 <1937525930@qq.com>
2017-05-05 09:26:48 +0800

    更新反射方法。

--- CommontUtils/src/main/java/com/afra55/commontutils/sys/ReflectionUtil.java
+++ CommontUtils/src/main/java/com/afra55/commontutils/sys/ReflectionUtil.java
@@ -2,7 +2,6 @@ package com.afra55.commontutils.sys;
 
 import android.text.TextUtils;
 
-
 import com.afra55.commontutils.log.LogUtils;
 
 import java.lang.reflect.Field;
@@ -36,6 +35,38 @@ public class ReflectionUtil {
                 }
             }
             Method method = clazz.getMethod(methodName, paramTypes);
+            method.setAccessible(true);
+            return method.invoke(obj, params);
+        } catch (NoSuchMethodException e) {
+            LogUtils.i("reflect", "method " + methodName + " not found in " + obj.getClass().getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 通过类对象，运行指定方法
+     * @param obj 类对象
+     * @param methodName 方法名
+     * @param params 参数值
+     * @return 失败返回null
+     */
+    public static Object invokeDeclaredMethod(Object obj, String methodName, Object[] params) {
+        if (obj == null || TextUtils.isEmpty(methodName)) {
+            return null;
+        }
+
+        Class<?> clazz = obj.getClass();
+        try {
+            Class<?>[] paramTypes = null;
+            if (params != null) {
+                paramTypes = new Class[params.length];
+                for (int i = 0; i < params.length; ++i) {
+                    paramTypes[i] = params[i].getClass();
+                }
+            }
+            Method method = clazz.getDeclaredMethod(methodName, paramTypes);
             method.setAccessible(true);
             return method.invoke(obj, params);
         } catch (NoSuchMethodException e) {
