dff8b4e9d0089eb023ddb75c45a0af90c150958e
Afra55 <1937525930@qq.com>
2017-05-12 17:05:16 +0800

    BaseBean.

--- APIModule/src/main/java/com/afra55/apimodule/bean/BaseBean.java
+++ APIModule/src/main/java/com/afra55/apimodule/bean/BaseBean.java
@@ -1,9 +1,17 @@
 package com.afra55.apimodule.bean;
 
+import com.alibaba.fastjson.JSON;
+
 import java.io.Serializable;
 
 /**
  * Created by Victor Yang on 2016/7/1.
  */
 public class BaseBean implements Serializable{
+
+    @Override
+    public String toString() {
+        return JSON.toJSONString(this);
+    }
+
 }
